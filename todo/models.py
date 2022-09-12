13e987a679325d79781eb9112782009de4f5b484
otecart <otecart@github.com>
2022-09-13 01:10:59 +0700

    dev Turn off ToDo's ownership temporarily

--- todo/models.py
+++ todo/models.py
@@ -5,13 +5,21 @@ User = get_user_model()
 
 
 class ToDo(models.Model):
-    title = models.CharField(max_length=200)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todo_lists")
+    title = models.CharField(max_length=200, default="To-Do List")
+    owner = models.ForeignKey(
+        User, null=True, on_delete=models.CASCADE, related_name="todo_lists"
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
 
 
 class ToDoEntry(models.Model):
     list = models.ForeignKey(ToDo, on_delete=models.CASCADE, related_name="entries")
     text = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.text
