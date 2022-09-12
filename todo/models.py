0f41b511a82448eb3a2813b21d4a30e4cb58b8b9
otecart <otecart@github.com>
2022-09-12 23:40:55 +0700

    dev Create models ToDo, ToDoEntry

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class ToDo(models.Model):
+    title = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todo_lists")
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class ToDoEntry(models.Model):
+    list = models.ForeignKey(ToDo, on_delete=models.CASCADE, related_name="entries")
+    text = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
