e3c65a4c2b6f2d00e17be2c26b6ef0698f24f5ef
otecart <otecart@github.com>
2022-09-13 01:12:28 +0700

    dev Add basic functionality for ToDo

--- todo/urls.py
+++ todo/urls.py
@@ -1,3 +1,14 @@
 from django.urls import path
 
-urlpatterns = []
+from .views import (ToDoCreateView, ToDoDeleteView, ToDoDetailView,
+                    ToDoEditView, ToDoListView)
+
+app_name = "todo"
+
+urlpatterns = [
+    path("", ToDoListView.as_view(), name="todo-list"),
+    path("create/", ToDoCreateView.as_view(), name="todo-create"),
+    path("<int:pk>/", ToDoDetailView.as_view(), name="todo-detail"),
+    path("<int:pk>/edit/", ToDoEditView.as_view(), name="todo-edit"),
+    path("<int:pk>/delete/", ToDoDeleteView.as_view(), name="todo-delete"),
+]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 
-# Create your views here.
+from .mixins import AddOwnerMixin
+from .models import ToDo, ToDoEntry
+
+
+class ToDoListView(ListView):
+    model = ToDo
+
+
+class ToDoDetailView(DetailView):
+    model = ToDo
+
+
+class ToDoCreateView(AddOwnerMixin, CreateView):
+    model = ToDo
+    fields = ["title"]
+    success_url = reverse_lazy("todo:todo-list")
+
+
+class ToDoEditView(UpdateView):
+    model = ToDo
+    fields = ["title"]
+
+    def get_success_url(self) -> str:
+        return reverse("todo:todo-detail", args=(self.object.pk,))
+
+
+class ToDoDeleteView(DeleteView):
+    model = ToDo
+    success_url = reverse_lazy("todo:todo-list")
