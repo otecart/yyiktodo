3cfca1b756f55fc1f4d23d165ebdd585d8bb65b4
otecart <otecart@github.com>
2022-09-14 21:52:09 +0700

    dev Add Login support

--- yyiktodo/settings.py
+++ yyiktodo/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "yyiktodo.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
