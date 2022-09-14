62c6d7408cac0ff4f08dc465f7f2cd1541206f51
otecart <otecart@github.com>
2022-09-14 21:53:42 +0700

    dev Create OwnedOnlyListMixin, OwnedOnlyDetailMixin

--- todo/mixins.py
+++ todo/mixins.py
@@ -1,17 +1,32 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.views.generic import View
+from django.views.generic.detail import SingleObjectMixin
 from django.views.generic.edit import ModelFormMixin
+from django.views.generic.list import MultipleObjectMixin
 
 
-# class AddOwnerMixin(LoginRequiredMixin, ModelFormMixin):
-class AddOwnerMixin(ModelFormMixin):
+class AddOwnerMixin(LoginRequiredMixin, ModelFormMixin):
     """Add current user as owner of created object."""
 
     def form_valid(self, form):
-        obj = form.save(commit=False)
-        user = self.request.user
-        if user.is_authenticated:
-            obj.owner = user
-        else:
-            obj.owner = None
-        obj.save()
-        return super().form_valid(form)
+        self.object = form.save(commit=False)
+        self.object.owner = self.request.user  # type: ignore
+        self.object.save()
+        return redirect(self.get_success_url())
+
+
+class OwnedOnlyListMixin(MultipleObjectMixin, View):
+    """Returns only user owned list of objects."""
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return self.model._default_manager.filter(owner=self.request.user)  # type: ignore
+        return self.model._default_manager.none()  # type: ignore
+
+
+class OwnedOnlyDetailMixin(LoginRequiredMixin, SingleObjectMixin, View):
+    """Returns only user owned object."""
+
+    def get_queryset(self):
+        return self.model._default_manager.filter(owner=self.request.user)
