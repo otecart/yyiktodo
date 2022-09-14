3f46ad80b1ac3c8b5e01c76851f45910f1b5da58
otecart <otecart@github.com>
2022-09-14 23:01:30 +0700

    dev Return NOT NULL constraint to ToDo, Add auto update for ToDo on ToDoEntry updates

--- todo/models.py
+++ todo/models.py
@@ -6,9 +6,7 @@ User = get_user_model()
 
 class ToDo(models.Model):
     title = models.CharField(max_length=200, default="To-Do List")
-    owner = models.ForeignKey(
-        User, null=True, on_delete=models.CASCADE, related_name="todo_lists"
-    )
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todo_lists")
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
@@ -20,6 +18,14 @@ class ToDoEntry(models.Model):
     todo = models.ForeignKey(ToDo, on_delete=models.CASCADE, related_name="entries")
     text = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+
+    def save(self):
+        self.todo.save()
+        return super().save()
+
+    def delete(self):
+        self.todo.save()
+        return super().delete()
 
     def __str__(self) -> str:
         return self.text
