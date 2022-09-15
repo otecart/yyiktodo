bfb62558da8bc4e9fa346af196e5077cb973de2a
otecart <otecart@github.com>
2022-09-16 01:10:19 +0700

    dev Remade custom mixins to more generic ones

--- todo/mixins.py
+++ todo/mixins.py
@@ -1,6 +1,10 @@
+from functools import reduce
+from operator import or_
+from typing import Callable
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q, QuerySet
 from django.shortcuts import redirect
-from django.views.generic import View
 from django.views.generic.detail import SingleObjectMixin
 from django.views.generic.edit import ModelFormMixin
 from django.views.generic.list import MultipleObjectMixin
@@ -16,17 +20,43 @@ class AddOwnerMixin(LoginRequiredMixin, ModelFormMixin):
         return redirect(self.get_success_url())
 
 
-class OwnedOnlyListMixin(MultipleObjectMixin, View):
-    """Returns only user owned list of objects."""
+class FilteredBaseMixin:
+    """Base mixin for filtering querysets."""
+
+    filters: list[Q]
+    filter_chainer: Callable[[Q, Q], Q]
+
+    def get_filters(self):
+        return self.filters
+
+
+class OrFilteredMixin(FilteredBaseMixin):
+    """Semi-base mixin for filtering querysets with `|`."""
+
+    filter_chainer = or_
+
+
+class FilteredSingleMixin(FilteredBaseMixin, SingleObjectMixin):
+    """Add queryset filtering for single object."""
 
     def get_queryset(self):
-        if self.request.user.is_authenticated:
-            return self.model._default_manager.filter(owner=self.request.user)  # type: ignore
-        return self.model._default_manager.none()  # type: ignore
+        queryset = super().get_queryset()
+        total_filter = reduce(self.filter_chainer, self.get_filters())
+        return queryset.filter(total_filter)
 
 
-class OwnedOnlyDetailMixin(LoginRequiredMixin, SingleObjectMixin, View):
-    """Returns only user owned object."""
+class OrFilteredSingleMixin(OrFilteredMixin, FilteredSingleMixin):
+    """Add queryset filtering with `|` for single object."""
+
+
+class FilteredMultipleMixin(FilteredBaseMixin, MultipleObjectMixin):
+    """Add queryset filtering for multiple object."""
 
     def get_queryset(self):
-        return self.model._default_manager.filter(owner=self.request.user)
+        queryset: QuerySet = super().get_queryset()  # type: ignore
+        total_filter = reduce(self.filter_chainer, self.get_filters())
+        return queryset.filter(total_filter)
+
+
+class OrFilteredMultipleMixin(OrFilteredMixin, FilteredMultipleMixin):
+    """Add queryset filtering with `|` for multiple object."""
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic import (
@@ -10,60 +12,91 @@ from django.views.generic import (
 )
 
 from .forms import EntryForm
-from .mixins import AddOwnerMixin, OwnedOnlyDetailMixin, OwnedOnlyListMixin
+from .mixins import AddOwnerMixin, OrFilteredMultipleMixin, OrFilteredSingleMixin
 from .models import ToDo, ToDoEntry
 
 
-class ToDoListView(OwnedOnlyListMixin, ListView):
+class ToDoListView(OrFilteredMultipleMixin, ListView):
     model = ToDo
 
+    def get_filters(self):
+        filters = [Q(public=True)]
+        if self.request.user.is_authenticated:
+            filters.append(Q(owner=self.request.user))
+        return filters
 
-class ToDoDetailView(OwnedOnlyDetailMixin, DetailView):
+
+class MyToDoListView(LoginRequiredMixin, OrFilteredMultipleMixin, ListView):
+    model = ToDo
+
+    def get_filters(self):
+        return [Q(owner=self.request.user)]
+
+
+class ToDoDetailView(OrFilteredSingleMixin, DetailView):
     model = ToDo
     extra_context = {"entry_form": EntryForm()}
+
+    def get_filters(self):
+        filters = [Q(public=True)]
+        if self.request.user.is_authenticated:
+            filters.append(Q(owner=self.request.user))
+        return filters
 
 
 class ToDoCreateView(AddOwnerMixin, CreateView):
     model = ToDo
-    fields = ["title"]
+    fields = ["title", "public"]
     success_url = reverse_lazy("todo:todo-list")
 
 
-class ToDoEditView(OwnedOnlyDetailMixin, UpdateView):
+class ToDoEditView(LoginRequiredMixin, OrFilteredSingleMixin, UpdateView):
     model = ToDo
-    fields = ["title"]
+    fields = ["title", "public"]
+
+    def get_filters(self):
+        return [Q(owner=self.request.user)]
 
     def get_success_url(self) -> str:
         return reverse("todo:todo-detail", args=(self.object.pk,))
 
 
-class ToDoDeleteView(OwnedOnlyDetailMixin, DeleteView):
+class ToDoDeleteView(LoginRequiredMixin, OrFilteredSingleMixin, DeleteView):
     model = ToDo
     success_url = reverse_lazy("todo:todo-list")
 
+    def get_filters(self):
+        return [Q(owner=self.request.user)]
 
-class EntryCreateView(View):
+
+class EntryCreateView(LoginRequiredMixin, View):
     def post(self, request, pk: int):
-        todo = get_object_or_404(ToDo, pk=pk)
+        todo = get_object_or_404(ToDo, pk=pk, owner=self.request.user)
         entry = ToDoEntry(todo=todo, text=request.POST["text"])
         entry.save()
         return redirect(reverse("todo:todo-detail", args=(todo.pk,)))
 
 
-class EntryEditView(UpdateView):
+class EntryEditView(LoginRequiredMixin, OrFilteredSingleMixin, UpdateView):
     model = ToDoEntry
     fields = ["text", "completed"]
     template_name = "todo/entry_form.html"
     context_object_name = "entry"
 
+    def get_filters(self):
+        return [Q(todo__owner=self.request.user)]
+
     def get_success_url(self) -> str:
         return reverse("todo:todo-detail", args=(self.object.todo.pk,))
 
 
-class EntryDeleteView(DeleteView):
+class EntryDeleteView(LoginRequiredMixin, OrFilteredSingleMixin, DeleteView):
     model = ToDoEntry
     template_name = "todo/entry_confirm_delete.html"
     context_object_name = "entry"
+
+    def get_filters(self):
+        return [Q(todo__owner=self.request.user)]
 
     def get_success_url(self) -> str:
         return reverse("todo:todo-detail", args=(self.object.todo.pk,))
