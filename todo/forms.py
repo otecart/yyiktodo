cf451d595ccc5a22080446c949314fb768e72f03
otecart <otecart@github.com>
2022-09-18 14:13:00 +0700

    dev Connect Bootstrap and Crispy Forms

--- todo/forms.py
+++ todo/forms.py
@@ -4,7 +4,11 @@ from .models import ToDoEntry
 
 
 class EntryForm(forms.ModelForm):
-    text = forms.CharField(min_length=2, max_length=200)
+    text = forms.CharField(
+        min_length=2,
+        max_length=200,
+        label="",
+    )
 
     class Meta:
         model = ToDoEntry
--- yyiktodo/settings.py
+++ yyiktodo/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # installed
     "debug_toolbar",
+    "crispy_forms",
+    "crispy_bootstrap5",
     # my apps
     "todo",
 ]
@@ -122,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -129,3 +134,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 INTERNAL_IPS = ["127.0.0.1"]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
